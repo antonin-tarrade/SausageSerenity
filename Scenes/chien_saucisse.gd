ff5e347dd24c135e34ccc0de8f268dd4a55a3f31
Telessia <dunty.telessia@gmail.com>
2024-01-27 12:18:56 +0100

    extension

--- Scenes/chien_saucisse.gd
+++ Scenes/chien_saucisse.gd
@@ -1,18 +1,20 @@
 extends CharacterBody2D
 
 @export var SPEED = 300.0
-@export var EXTENSION_SPEED = 1 # px
-@export var SHRINKING_SPEED = 2 # px
+@export var EXTENSION_SPEED = 2 # px
+@export var SHRINKING_SPEED = 4 # px
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var a_tree: AnimationTree = $AnimationTree
 @onready var c_shape: CollisionShape2D = $CollisionShape2D
-@onready var c_rect: ColorRect = $ColorRect
+
+const normal_size: Vector2 = Vector2(32, 24)
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var turning_left: bool = false
+var extended: bool = false
 enum extending_direction {LEFT, RIGHT, UP, DOWN}
-var ed: int = 0
+var ed: int = -1
 
 func _ready():
 	a_tree.set("parameters/conditions/idle", is_on_floor() and (velocity.x == 0))
@@ -24,13 +26,18 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 		
 	if Input.is_action_pressed("extend"):
-		if ed == 0:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if ed == -1:
 			var horizontal: float = Input.get_axis("ui_left", "ui_right")
 			var vertical: float = Input.get_axis("ui_down", "ui_up")
 			ed = get_extending_direction(horizontal, vertical)
+			print(ed)
 		else:
 			extension(ed)
 	else:
+		
+		if extended:
+			pass
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var direction = Input.get_axis("ui_left", "ui_right")
@@ -55,18 +62,20 @@ func get_extending_direction(horizontal: float, vertical: float) -> int:
 	elif vertical < 0:
 		return extending_direction.DOWN
 	else:
-		return 0
+		return -1
 		
 # extends the dog in the right direction
 func extension(ed: int) -> void:
 	match ed:
 		extending_direction.RIGHT:
-			print("right")
 			c_shape.shape.size.x += EXTENSION_SPEED
-			c_rect.size.x += EXTENSION_SPEED
+			c_shape.position.x += EXTENSION_SPEED / 2
 		extending_direction.LEFT:
-			print("left")
 			c_shape.shape.size.x += EXTENSION_SPEED
-			c_rect.size.x += EXTENSION_SPEED
-			c_shape.position.x -= EXTENSION_SPEED
-			c_rect.size.x -= EXTENSION_SPEED
+			c_shape.position.x -= EXTENSION_SPEED / 2
+		extending_direction.DOWN:
+			print("not yet implemented")
+		extending_direction.UP:
+			c_shape.position.y -= EXTENSION_SPEED / 2
+			c_shape.shape.size.y += EXTENSION_SPEED
+

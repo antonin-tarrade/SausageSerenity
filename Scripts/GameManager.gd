d95d053122f40e4bf2b43a8b3d5f6ba25aa35aaf
Floriane Payen <payenf@bde.enseeiht.fr>
2024-01-28 15:50:15 +0100

    shaaader

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,10 +1,10 @@
 class_name GameManager extends Node2D
 
-@export var nbMissions : int = 11
-var nbMissionsFinies : int = 5
+@export var nbMissions : float = 11.0
+var nbMissionsFinies : float = 0.0
 
 func _on_mission_fini():
-	nbMissionsFinies += 1
+	nbMissionsFinies += 1.0
 
 func get_pourcentage_fini() -> float :
 	return nbMissionsFinies/nbMissions

f6e17e51bb37981ba764e35dadcb6aee6a8611f3
Floriane Payen <payenf@bde.enseeiht.fr>
2024-02-08 14:57:13 +0100

    mot fin kitch

--- Scenes/Mission_finale.gd
+++ Scenes/Mission_finale.gd
@@ -3,7 +3,7 @@ extends Sprite2D
 func verifier(personnage : Personnage) -> bool :
 	print("mission finale")
 	if personnage.gm.get_pourcentage_fini() >= 0.98 :
-		
+		personnage.gm.fin_ultime()
 		return true
 	return false
 
--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -3,8 +3,14 @@ class_name GameManager extends Node2D
 @export var nbMissions : float = 11.0
 var nbMissionsFinies : float = 0.0
 
+func _ready():
+	%LayerFin.visible = false
+
 func _on_mission_fini():
 	nbMissionsFinies += 1.0
 
 func get_pourcentage_fini() -> float :
 	return nbMissionsFinies/nbMissions
+
+func fin_ultime():
+	%LayerFin.visible = true
